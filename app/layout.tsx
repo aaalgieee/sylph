d4982a8ba2f0f3bff11eadc3f4783336c0e2deaa
Al Gabriel <jaworski.orig@gmail.com>
2024-11-12 17:24:49 +0800

    fix: speed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
           <main className="mx-auto max-w-screen-sm overflow-x-hidden px-6 py-24 md:overflow-x-visible ">
             <article className="article">{children}</article>
           </main>
+          <SpeedInsights />
         </Providers>
-        <SpeedInsights />
       </body>
     </html>
   );
